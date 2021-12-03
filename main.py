4b1cd7ca359d9afff1fe0710eea90f4b41639ce6
evgeniya35 <evgeniya35@yandex.ru>
2021-12-03 16:48:40 +0300

    add except

--- main.py
+++ main.py
@@ -2,16 +2,26 @@ import logging
 import os
 
 import requests
+from requests.models import HTTPError
 
 logger = logging.getLogger('logger_main')
+
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError
+
 
 def load_book(id, filename):
     url = f'https://tululu.org/txt.php?id={id}'
     response = requests.get(url)
     response.raise_for_status()
-    with open(filename, 'wb') as file:
-        file.write(response.content)
-    logger.info(f'Download book {id}')
+    try:
+        check_for_redirect(response)
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+        logger.info(f'Download book {id}')
+    except HTTPError:
+        pass
 
 
 def main():
