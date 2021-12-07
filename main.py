c1101fb59825e9a637ab3fa23e0a9b49018f6fdf
evgeniya35 <evgeniya35@yandex.ru>
2021-12-07 21:17:31 +0300

    download book covers

--- main.py
+++ main.py
@@ -1,32 +1,47 @@
 import logging
 import os
 
-
 import requests
 
+from urllib.parse import urljoin, urlparse
+
 from pathvalidate import sanitize_filename
-from requests.models import HTTPError
+from requests.models import HTTPError, Response
 from bs4 import BeautifulSoup
 
+
 logger = logging.getLogger('logger_main')
+
 
 def check_for_redirect(response):
     if response.history:
         raise HTTPError
 
 
-def book_title_author(id):
+def parse_book_page(id):
     url = f'https://tululu.org/b{id}'
     response = requests.get(url)
     response.raise_for_status()
     soup = BeautifulSoup(response.text, 'lxml')
     title, author = (el.strip() for el in soup.find('h1').text.split('::'))
-    return title, author
+    img = urljoin(url, soup.find('div', class_='bookimage').find('img')['src'])
+    return title, img
 
 
 def make_filename(id, title, folder='books'):
     filename = f'{id}. {sanitize_filename(title)}.txt'
     return os.path.join(folder, filename)
+
+
+def load_cover(url, folder='books'):
+    filename = os.path.join(
+        folder,
+        urlparse(url).path.split('/')[-1]
+    )
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+        file.write(response.content)
 
 
 def load_book(id):
@@ -35,11 +50,12 @@ def load_book(id):
     response.raise_for_status()
     try:
         check_for_redirect(response)
-        title, author = book_title_author(id)
+        title, img = parse_book_page(id)
+        load_cover(img)
         filename = make_filename(id, title)
         with open(filename, 'wb') as file:
             file.write(response.content)
-        logger.info(f'Save book {filename}')
+        logger.info(f'Save book {filename} {img}')
     except HTTPError:
         pass
 
